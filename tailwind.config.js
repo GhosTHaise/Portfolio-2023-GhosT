1407329331d2645e530401de834c4ae3e2d82c85
GhosTHaise <Ghostrex2@gmail.com>
2023-01-31 18:55:34 +0300

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  mode : "jit",
+  theme: {
+    extend: {
+      colors : {
+        primary : "#edf2f8",
+        secondary : "#313bac",
+        blackColor : "#030303",
+        lightGray : "#e4e4e4",
+        grayColor : "6b7688",
+        brownColor:"#46364a",
+        whiteColor : "#ffffff"
+      },
+    },
+    screens :{
+      xs : "480px",
+      ss : "620px",
+      sm : "768px",
+      md : "1060px",
+      lg : "1200px",
+      xl : "1700px"
+    },
+  },
+  plugins: [],
+}
