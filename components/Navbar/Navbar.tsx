cc9d1b64236d1455cd99f338ee622a8585537af5
GhosTHaise <Ghostrex2@gmail.com>
2023-02-03 17:09:33 +0300

    fix new next13 import module.css

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import {images,variable} from "../../constants";
-import "./Navbar.module.scss";
+import styles from "./Navbar.module.scss";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
+    <nav className={styles.app__navbar}>
+      <div className={styles.app__navbar_logo}>
         <img 
         src={images.logo.src} 
         alt="logo" 
         className="" />
       </div>
-      <ul className='app__navbar-links'>
+      <ul className={styles.app__navbar_links}>
         {variable.navbarConstants.map((item,index)=>(
-          <li className='app__flex p-text' key={`link-${index}`}>
+          <li className={styles.app__flex && styles.p_text} key={`link-${index}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Filter out default Next.js CSS rules:
+    config.module.rules = config.module.rules.filter(r => !r.oneOf);
+
+    // Add your own rules
+
+    config.module.rules.push({
+      test: /\.scss$/i,
+      use: ['style-loader', 'css-loader'],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
