91613b808a95631612a9ceb11bc4674e798e6a6e
GhosTHaise <Ghostrex2@gmail.com>
2023-02-03 16:58:44 +0300

    combine sass and tailwind

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import {images,variable} from "../../constants";
+import "./Navbar.module.scss";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img 
         src={images.logo.src} 
         alt="logo" 
         className="" />
       </div>
-      <ul>
+      <ul className='app__navbar-links'>
         {variable.navbarConstants.map((item,index)=>(
-          <li key={`link-${index}`}>
+          <li className='app__flex p-text' key={`link-${index}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
