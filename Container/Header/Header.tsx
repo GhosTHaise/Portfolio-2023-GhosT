5b31321b36163d097417c90783883bc64acc9dbf
GhosTHaise <Ghostrex2@gmail.com>
2023-02-04 20:38:07 +0300

    init Header section

--- Container/Header/Header.tsx
+++ Container/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import {motion as m} from "framer-motion";
+import {images} from "../../constants";
+import styles from "./Header.module.scss";
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <div>Header</div>
+    <div className={styles.app__Header && styles.app__flex}>
+        <m.div
+          whileInView={{x : [-100,0],opacity : [0,1]}}
+          transition={{duration : .5}}
+          className={styles.app__header_info}
+        >
+            
+        </m.div>
+    </div>
   )
 }
 
