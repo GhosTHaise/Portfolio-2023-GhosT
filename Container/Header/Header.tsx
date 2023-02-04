d6605a4613ace504de6539b791fca44641dfad4f
GhosTHaise <Ghostrex2@gmail.com>
2023-02-04 23:28:00 +0300

    Start to build header

--- Container/Header/Header.tsx
+++ Container/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 import {motion as m} from "framer-motion";
 import {images} from "../../constants";
 import styles from "./Header.module.scss";
@@ -9,10 +10,44 @@ function Header({}: Props) {
     <div className={styles.app__Header && styles.app__flex}>
         <m.div
           whileInView={{x : [-100,0],opacity : [0,1]}}
-          transition={{duration : .5}}
+          transition={{duration : 0.5}}
           className={styles.app__header_info}
         >
-            
+            <div className={styles.app__header_badge}>
+                <div className={`app__flex ${styles.badge_cmp}`}>
+                      <span>
+
+                      </span>
+                      <div className='ml-[20]'>
+                          <p className="p-text">
+                            Hello,I am
+                          </p>
+                          <h1 className='head-text'>
+                                Fitiavana
+                          </h1>
+                      </div>
+                </div>
+                <div className={`${styles.tag_cmp} app__flex`}>
+                      <p className="p-text">
+                            Fullstack Developper
+                      </p>
+                      <p className="p-text">
+                            Freelancer
+                      </p>
+                </div>
+            </div>
+        </m.div>
+        <m.div
+        whileInView={{opacity : [0,1]}}
+        transition={{duration : 0.5,delayChildren : 0.5}}
+        className={styles.app__header_img}>
+            <Image 
+                src={images.profile.src} 
+                alt="profile_bg" 
+                className='' />
+        </m.div>
+        <m.div>
+
         </m.div>
     </div>
   )
