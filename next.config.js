ca18cb9854c4cebdec89ceb5597f41b60dcec9fe
GhosTHaise <Ghostrex2@gmail.com>
2023-02-04 11:34:52 +0300

    Fix sass loader

--- next.config.js
+++ next.config.js
@@ -1,18 +1,32 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Filter out default Next.js CSS rules:
-    config.module.rules = config.module.rules.filter(r => !r.oneOf);
-
-    // Add your own rules
-
-    config.module.rules.push({
-      test: /\.scss$/i,
-      use: ['style-loader', 'css-loader'],
-    })
-    return config
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
+  /* sassOptions: {
+    data: '@import "./scss/_variables.scss"',
+    sourceMap: true,
+  } */
 }
+
+/* Old Value */
+/* webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  // Filter out default Next.js CSS rules:
+  config.module.rules = config.module.rules.filter(r => !r.oneOf);
+
+  // Add your own rules
+
+  config.module.rules.push({
+    test: /\.scss$/i,
+    use: ['style-loader', 'css-loader'],
+  })
+  return config
+},
+sassOptions: {
+  includePaths: [path.join(__dirname, 'styles')],
+}, */
 
 module.exports = nextConfig
