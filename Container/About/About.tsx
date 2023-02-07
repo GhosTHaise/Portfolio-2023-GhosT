ea1a55cf92dcc9cf46404d969d1b1137679c07f4
GhosTHaise <Ghostrex2@gmail.com>
2023-02-08 00:24:07 +0300

    start to build about me container

--- Container/About/About.tsx
+++ Container/About/About.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { motion as m } from 'framer-motion'
+import { variable } from '@/constants';
+import "./About.module.scss";
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+          I Know That
+          <span>
+            Good Design
+          </span>
+          <br />
+              means
+          <span>
+            Good Business
+          </span>
+      </h2>
+
+      <div className={`app__profiles`}>
+            {variable.About.map((about,index)=>(
+              <m.div
+                key={`about-${index}`}
+                whileInView={{opacity : 1}}
+                whileHover={{scale : 1.1}}
+                transition={{duration : 0.5,type : "tween"}}
+                className={`app__profile-item`}
+              >
+                  <img 
+                  src={about.ImageUrl} 
+                  alt={about.title}  />
+
+                  <h2 className='bold-text' style={{marginTop : 20}}>
+                      {about.title}
+                  </h2>
+              </m.div>
+            ))}
+      </div>
+    </>
   )
 }
 
--- constants/variable.tsx
+++ constants/variable.tsx
@@ -6,5 +6,11 @@ export default {
         "work",
         "skills",
         "contact"
+    ],
+    About : [
+        { title : "Web Developmen" , description : "I am a good Web Developper",ImageUrl : ""},
+        { title : "Web Design" , description : "I am a good Web Developper",ImageUrl : ""},
+        { title : "Ui/UX" , description : "I am a good Web Developper",ImageUrl : ""},
+        { title : "Web Animation" , description : "I am a good Web Developper",ImageUrl : ""}
     ]
 }
