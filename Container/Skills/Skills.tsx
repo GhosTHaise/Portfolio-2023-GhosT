cbaaafd462ce17efdbf6ff093a694d9a0bae71e6
GhosTHaise <Ghostrex2@gmail.com>
2023-02-12 19:51:49 +0300

    Start to build skills section

--- Container/Skills/Skills.tsx
+++ Container/Skills/Skills.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {motion as m} from "framer-motion";
+import {Tooltip as ReactTooltip} from "react-tooltip"
+import { AppWrap } from '@/wrapper';
+import styles from "./Skills.module.scss";
+import { client,urlFor } from '@/client';
 
 type Props = {}
 
 function Skills({}: Props) {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([])
+  
+  const experienceQuery = `*[_type == "works"]`;
+  const skillsQuery = `[_type == "skills"]`;
+  
+  /* Fetch Experience */
+  useEffect(()=>{
+    client.fetch(experienceQuery).then( data => {
+      setExperience(data);
+    })
+  },[experienceQuery]);
+  
+  /* Fetch Skill */
+  useEffect(()=>{
+    client.fetch(skillsQuery).then( data => {
+      setSkills(data);
+    })
+  },[skillsQuery]);
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>
+          Skills & Experience
+      </h2> 
+      <div className={`${styles.app__sills_container}`}>
+          <m.div className={styles.app__skills_list}>
+              {skills.map((skill)=>(
+                <m.div
+                whileInView={{opacity : [0,1]}}
+                transition={{duration : 0.5}}
+                className={`${styles.app__skills_item} app__flex`}
+                key={skill.name}
+                >
+
+                </m.div>
+              )}
+          </m.div>
+      </div>
+    </>
   )
 }
 
