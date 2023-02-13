c52368ee1297fbcf373a168588ff9b400b8edeff
GhosTHaise <Ghostrex2@gmail.com>
2023-02-13 11:59:00 +0300

    Build professional experience section

--- Container/Skills/Skills.tsx
+++ Container/Skills/Skills.tsx
@@ -5,6 +5,18 @@ import { AppWrap } from '@/wrapper';
 import styles from "./Skills.module.scss";
 import { client,urlFor } from '@/client';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { type } from 'os';
+
+type workExperience = {
+  name : string,
+  company : string,
+  desc : string
+}
+
+type experience = {
+  year : string ,
+  works : workExperience[]
+}
 
 type skills = {
   name : string,
@@ -15,16 +27,16 @@ type skills = {
 type Props = {}
 
 function Skills({}: Props) {
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperiences] = useState<experience[]>([]);
   const [skills, setSkills] = useState<skills[]>([])
   
-  const experienceQuery = `*[_type == "works"]`;
+  const experienceQuery = `*[_type == "experiences"]`;
   const skillsQuery = `*[_type == "skills"]`;
   
   /* Fetch Experience */
   useEffect(()=>{
     client.fetch(experienceQuery).then( data => {
-      setExperience(data);
+      setExperiences(data);
     })
   },[experienceQuery]);
   
@@ -60,6 +72,52 @@ function Skills({}: Props) {
                 </m.div>
               ))}
           </m.div>
+          <div className={styles.app__skills_exp}>
+                {experiences?.map((experience,index) => 
+                  (
+                    <m.div 
+                    key={`${experience.year}-0${index}`}
+                    className={styles.app__skills_exp_item}
+                    >
+                        <div className={styles.app__skills_exp_year}>
+                              <p className='bold_text'>
+                                  {experience.year}
+                              </p>
+                        </div>
+                        <m.div 
+                        className={styles.app__skills_exp_works}
+                        >
+                            {experience?.works?.map((work,index)=>(
+                              <>
+                                <m.div
+                                  key={work.name}
+                                  whileInView={{opacity : [0,1]}}
+                                  transition={{duration : 0.5}}
+                                  className={styles.app__skills_exp_work}
+                                  data-tip 
+                                  data-for={work.name}
+                                  >
+                                    <h4 className="bold_text">
+                                          {work.name}
+                                    </h4>
+                                    <p className='p-text'>
+                                          {work.company}
+                                    </p>
+                                </m.div>
+                                <ReactTooltip
+                                  anchorId={work.name}
+                                  className={styles.skills_tooltip}
+                                  
+                                >
+                                    {work.desc}
+                                </ReactTooltip>
+                              </>
+                            ))}
+                        </m.div>
+                    </m.div>
+                  )
+                )}
+          </div>
       </div>
     </>
   )
