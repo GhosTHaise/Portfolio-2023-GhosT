13b294ac1be6efd0b573808a6aa83558a1ccb1df
GhosTHaise <Ghostrex2@gmail.com>
2023-02-15 18:13:26 +0300

    Crete form to save message

--- Container/Footer/Footer.tsx
+++ Container/Footer/Footer.tsx
@@ -7,6 +7,29 @@ import styles from "./Footer.module.scss";
 type Props = {}
 
 function Footer({}: Props) {
+
+  const [formData, setFormData] = useState<{name : string,email : string,message : string}>(
+    { name : "",email : "",message : ""}
+  );
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false); 
+  const [loading, setLoading] = useState(false);
+
+  const handleChangeinput = (e : React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>  ) => {
+    const {name  , value} = e.target;
+    setFormData({...formData,[name] : value});
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+      const contact = {
+        _type : "contact",
+        ...formData
+      }
+
+      console.log(contact);
+    setLoading(false);
+  }
+
   return (
     <>
         <h2 className='head-text'>
@@ -31,6 +54,39 @@ function Footer({}: Props) {
                   +261 33 64 986 49
                 </a>
             </div>
+        </div>
+        <div className={`${styles.app__footer_form} app__flex `}>
+            <div className='app__flex'>
+                  <input 
+                      className='p-text' 
+                      type="text" 
+                      placeholder='Your Name' 
+                      name='name'
+                      value={formData.name} onChange={handleChangeinput}   />
+            </div>
+            <div className='app__flex'>
+                  <input 
+                      className='p-text' 
+                      type="email" 
+                      placeholder='Your Email' 
+                      name='email'
+                      value={formData.email} onChange={handleChangeinput}   />
+            </div>
+            <div>
+                <textarea 
+                    className='p-text'
+                    placeholder='Your Message'
+                    name="message"
+                    value={formData.message} onChange={handleChangeinput}
+                />
+            </div>
+            <button
+              type='button'
+              className='p-text'
+              onClick={handleSubmit}
+              >
+                {loading ? "Sending ..." : "Send Message"}
+            </button>
         </div>
     </>
   )
