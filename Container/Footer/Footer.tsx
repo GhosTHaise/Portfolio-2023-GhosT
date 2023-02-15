53582ff7d57a65fd7d497db7551c619cf7dcae45
GhosTHaise <Ghostrex2@gmail.com>
2023-02-15 17:13:47 +0300

    add email and phone number

--- Container/Footer/Footer.tsx
+++ Container/Footer/Footer.tsx
@@ -8,7 +8,31 @@ type Props = {}
 
 function Footer({}: Props) {
   return (
-    <div>Footer</div>
+    <>
+        <h2 className='head-text'>
+              Take a coffee & chat with me
+        </h2>
+        <div className={styles.app__footer_cards}>
+            <div className={styles.app__footer_card}>
+                <img 
+                src={images.email.src} 
+                alt="email" />
+
+                <a href="mailto:Ghostrex2@gmail.com" className='p-text'>
+                  Ghostrex2@gmail.com
+                </a>
+            </div>
+            <div className={styles.app__footer_card}>
+                <img 
+                src={images.mobile.src} 
+                alt="email" />
+
+                <a href="tel: +261 (33) 64-986-49" className='p-text'>
+                  +261 33 64 986 49
+                </a>
+            </div>
+        </div>
+    </>
   )
 }
 
